af78edf60b1f3456c804c1ce591d2c1d4fc06b86
DavHau <hsngrmpf+github@gmail.com>
2022-11-30 14:07:18 +0700

    drv-parts.nix: add comment about populating outputs

--- modules/drv-parts.nix
+++ modules/drv-parts.nix
@@ -19,6 +19,18 @@ in {
     });
 
   config.perSystem = {config, pkgs, ...}: {
+    /*
+      TODO: I'm not sure yet if we should expose just the evaled module instead.
+      The evaled module is also a valid derivation because we set
+        `type = "derivation"` and `drvPath`, but it is currently missing
+        attributes like `overrideAttrs` or `out`, etc.
+    */
+    # config.packages = config.pkgs;
+
+    /*
+      This exposes the `.derivation` attribute (the actual derivation) of each
+        defined `pkgs.xxx` under the flake output `packages`.
+    */
     config.packages = l.mapAttrs (name: pkg: pkg.derivation) config.pkgs;
   };
 
