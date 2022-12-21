0ec2cf078ee7e7f034e61a5c9568f485b9919a7e
DavHau <hsngrmpf+github@gmail.com>
2022-12-21 20:56:48 +0700

    feat: add ./docs.nix

---
+++ docs.nix
@@ -0,0 +1,17 @@
+{
+  pkgs ? import <nixpkgs> {},
+  drv-parts ? import ./default.nix {inherit (pkgs) lib;},
+}: let
+  l = pkgs.lib // builtins;
+
+  evalModules = modules: l.evalModules {inherit modules;};
+
+  getOptions = modules: (evalModules modules).options;
+
+  getDocs = module: pkgs.nixosOptionsDoc {
+    options = getOptions [module];
+  };
+
+  docs = l.mapAttrs (_: getDocs) drv-parts.drv-backends;
+in
+  docs
