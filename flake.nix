8f1a46cd2fcc8c5b129a5c6dbe438d10fe2f267f
DavHau <hsngrmpf+github@gmail.com>
2023-02-18 17:58:46 +0700

    feat(tests): allow to exclude tests via find args

--- flake.nix
+++ flake.nix
@@ -30,20 +30,20 @@
       perSystem = {system, pkgs, ...}: {
         packages.tests-examples = pkgs.writeShellScriptBin "tests-examples" ''
           set -eu -o pipefail
-          for example in $(find ./examples/flake-parts/ -type f); do
+          for example in $(find ./examples/flake-parts/ -type f "$@"); do
             echo "building example $example"
             nix flake check "$example" -L \
               --show-trace \
               --no-write-lock-file \
               --override-input drv-parts ${self}
           done
-          for example in $(find ./examples/no-flake/ -type f); do
+          for example in $(find ./examples/no-flake/ -type f "$@"); do
             echo "building example $example"
             nix build -f "$example" -L \
               --show-trace \
               --no-link
           done
-          for example in $(find ./tests/ -type f); do
+          for example in $(find ./tests/ -type f "$@"); do
             echo "building test $example"
             nix build -f "$example" -L \
               --show-trace \
