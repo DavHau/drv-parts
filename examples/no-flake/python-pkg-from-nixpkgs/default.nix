9e0b36c278942e1612b68e3d2fa4b876fe398adc
DavHau <hsngrmpf+github@gmail.com>
2023-02-18 19:23:52 +0700

    feat: add example for importing python packages from nixpkgs

---
+++ examples/no-flake/python-pkg-from-nixpkgs/default.nix
@@ -0,0 +1,28 @@
+{
+  nixpkgs ? import <nixpkgs> {},
+  drv-parts ? import ../../../default.nix {inherit (nixpkgs) lib;},
+  ...
+}: let
+  l = nixpkgs.lib // builtins;
+
+  # use makeModule to make a module out of applications/misc/hello/default.nix
+  pythonModule = drv-parts.lib.makeModule
+    (nixpkgs.path + /pkgs/development/python-modules/requests/default.nix);
+
+  # define another module to set `deps`
+  myModule = {
+
+    deps = {nixpkgs, ...}: {config = nixpkgs // nixpkgs.python3.pkgs;};
+
+    stdenv = nixpkgs.stdenv;
+  };
+
+  pkg = l.evalModules {
+    modules= [
+      pythonModule
+      myModule
+    ];
+    specialArgs.dependencySets.nixpkgs = nixpkgs;
+  };
+in
+  pkg.config.final.derivation
