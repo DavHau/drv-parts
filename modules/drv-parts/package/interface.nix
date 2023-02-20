79cc2d60cf5574bdd3e63f5fe4f62ee33f3457d9
DavHau <hsngrmpf+github@gmail.com>
2023-02-20 21:37:05 +0700

    fix: final.package option declarations wrong

--- modules/drv-parts/package/interface.nix
+++ modules/drv-parts/package/interface.nix
@@ -2,20 +2,19 @@
   l = lib // builtins;
   t = l.types;
 
-  opts = {
-    # this will contain the resulting derivation
-    final.package = let
-      optsPackage = import ./optsPackage.nix {
-        inherit lib;
-        inherit (config) outputs;
-      };
-      optsPackageCompat = import ./optsPackageCompat.nix {inherit lib;};
-      optsPackageDrvParts = import ./optsPackageDrvParts.nix {inherit lib;};
-    in
-      optsPackage // optsPackageCompat // optsPackageDrvParts;
+  opts = let
+    optsPackage = import ./optsPackage.nix {
+      inherit lib;
+      inherit (config) outputs;
+    };
+    optsPackageCompat = import ./optsPackageCompat.nix {inherit lib;};
+    optsPackageDrvParts = import ./optsPackageDrvParts.nix {inherit lib;};
+  in
+    optsPackage // optsPackageCompat // optsPackageDrvParts;
 
-  };
 in {
+
+  # this will contain the resulting derivation
   options.final.package = l.mkOption {
     type = t.submodule {
       freeformType = t.lazyAttrsOf t.anything;
