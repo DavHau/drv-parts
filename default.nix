2544be2a9e7c8f347a3af98ba0298f2ee9d3f0fa
DavHau <hsngrmpf+github@gmail.com>
2023-03-24 12:16:38 +0800

    chore(default.nix): remove args

--- default.nix
+++ default.nix
@@ -1,16 +1,16 @@
 # This file provides backward compatibility to nix < 2.4 clients
-
-args:
 let
   flake =
     import
     (
-      let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-      fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-        sha256 = lock.nodes.flake-compat.locked.narHash;
-      }
+      let
+        lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      in
+        fetchTarball {
+          url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+          sha256 = lock.nodes.flake-compat.locked.narHash;
+        }
     )
-    { src = ./.; };
+    {src = ./.;};
 in
   flake.defaultNix
